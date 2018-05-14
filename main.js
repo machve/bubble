cb4d77955b6790565070c57d9fded2bef794fe0f
Veronika Machackova <veronikam@Veronikas-MacBook-Pro.local>
2018-05-14 11:51:51 +0200

    making bubbles move, give them random colors

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+const divs = document.querySelectorAll("#app div");
+
+divs.forEach(function (div) {
+    // div.style.transform = "translate (50 vw, 50vh)";
+    div.addEventListener("mouseenter", function () {
+        div.style.transform = `translate(${Math.random()*100}vw, ${Math.random()*100}vh)`;
+        div.style.backgroundColor =`hsl(${Math.random()*100},${Math.random()*100}%, ${Math.random()*100}%`;
+    })
+})
